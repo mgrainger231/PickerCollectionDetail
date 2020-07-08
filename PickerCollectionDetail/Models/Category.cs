295ca9f5233774d9bc96fb5a1309c73f078e606c
mgrainger <mgrainger231@gmail.com>
2020-07-07 22:41:14 -0400

    Adding missing files.

---
+++ PickerCollectionDetail/Models/Category.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using MvvmHelpers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace PickerCollectionDetail.Models
+{
+    public partial class Category : ObservableObject
+    {
+        private string id;
+        [JsonProperty("Id")]
+        public string Id
+        {
+            get { return id; }
+            set
+            {
+                SetProperty(ref id, value);
+            }
+        }
+
+        private string name;
+        [JsonProperty("Name")]
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                SetProperty(ref name, value);
+            }
+        }
+    }
+
+    public partial class Category
+    {
+        public static Category[] FromJson(string json) => JsonConvert.DeserializeObject<Category[]>(json, CategoryConverter.Settings);
+    }
+
+    public static class CategorySerialize
+    {
+        public static string ToJson(this Category[] self) => JsonConvert.SerializeObject(self, CategoryConverter.Settings);
+    }
+
+    internal static class CategoryConverter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+        {
+            new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+        },
+        };
+    }
+}
---
+++ PickerCollectionDetail/Services/MockCategoryDataStore.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PickerCollectionDetail.Models;
+
+namespace PickerCollectionDetail.Services
+{
+    public class MockCategoryDataStore : IDataStore<Category>
+    {
+        readonly List<Category> items;
+
+        public MockCategoryDataStore()
+        {
+            items = new List<Category>()
+            {
+                new Category { Id = "b3311ac3-ec93-413c-a6b3-f1b2f8cd7918", Name = "Cars" },
+                new Category { Id = "d4ad65ef-f083-46eb-98ed-623201887e59", Name = "Boats" }
+            };
+        }
+
+        public async Task<bool> AddItemAsync(Category item)
+        {
+            items.Add(item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateItemAsync(Category item)
+        {
+            var oldItem = items.Where((Category arg) => arg.Id == item.Id).FirstOrDefault();
+            items.Remove(oldItem);
+            items.Add(item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItemAsync(string id)
+        {
+            var oldItem = items.Where((Category arg) => arg.Id == id).FirstOrDefault();
+            items.Remove(oldItem);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<Category> GetItemAsync(string id)
+        {
+            return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
+        }
+
+        public async Task<IEnumerable<Category>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(items);
+        }
+        public async Task<IEnumerable<Category>> GetItemsAsync(string categoryId, bool forceRefresh = false)
+        {
+            return await Task.FromResult(items);
+        }
+    }
+}
